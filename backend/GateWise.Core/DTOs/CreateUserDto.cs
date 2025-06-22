3f7c1b34320ea7395afc2b81167dc2cf43a5831b
victor.dev <joaovictorbezerra.dev@gmail.com>
2025-06-22 02:08:20 -0400

    feat: add CreateUserDTO

---
+++ backend/GateWise.Core/DTOs/CreateUserDto.cs
@@ -0,0 +1,16 @@
+namespace GateWise.Core.DTOs;
+
+using GateWise.Core.Enums;
+
+public class CreateUserDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public int RegistrationNumber { get; set; }
+    public string UserAvatarUrl { get; set; } = string.Empty;
+    public GateWise.Core.Enums.UserType UserType { get; set; }
+    public string OperationalSystem { get; set; } = string.Empty;
+    public string OperationalSystemVersion { get; set; } = string.Empty;
+    public string DeviceModel { get; set; } = string.Empty;
+    public string DeviceManufactureName { get; set; } = string.Empty;
+}
