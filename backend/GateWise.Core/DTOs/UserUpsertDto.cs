fa7aa8b7baad10cfb677cceb456143973c4d5130
victor.dev <joaovictorbezerra.dev@gmail.com>
2025-07-12 14:27:44 -0400

    style: Change entity name to UserUpsertDto

--- backend/GateWise.Core/DTOs/UserUpsertDto.cs
+++ backend/GateWise.Core/DTOs/UserUpsertDto.cs
@@ -1,16 +1,17 @@
-namespace GateWise.Core.DTOs;
-
 using GateWise.Core.Enums;
 
-public class CreateUserDto
+namespace GateWise.Core.DTOs;
+
+public class UserUpsertDto
 {
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string? RegistrationNumber { get; set; }
     public string UserAvatarUrl { get; set; } = string.Empty;
-    public GateWise.Core.Enums.UserType UserType { get; set; }
+    public UserType UserType { get; set; }
     public string OperationalSystem { get; set; } = string.Empty;
     public string OperationalSystemVersion { get; set; } = string.Empty;
     public string DeviceModel { get; set; } = string.Empty;
     public string DeviceManufactureName { get; set; } = string.Empty;
+    public string? DevicePublicKeyPem { get; set; }
 }
