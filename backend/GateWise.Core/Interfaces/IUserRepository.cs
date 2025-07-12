7853bc649d78fbcdbaececebc6e5e285afcfb3e9
victor.dev <joaovictorbezerra.dev@gmail.com>
2025-07-12 15:04:35 -0400

    refactor: change UpdateAsync to receive user ID and DTO instead of entity

--- backend/GateWise.Core/Interfaces/IUserRepository.cs
+++ backend/GateWise.Core/Interfaces/IUserRepository.cs
@@ -1,3 +1,4 @@
+using GateWise.Core.DTOs;
 using GateWise.Core.Entities;
 
 namespace GateWise.Core.Interfaces;
@@ -7,7 +8,7 @@ public interface IUserRepository
     Task<List<User>> GetAllAsync();
     Task<User?> GetByIdAsync(string id);
     Task<User> CreateAsync(User user);
-    Task UpdateAsync(User user);
+    Task UpdateAsync(string id, UserUpsertDto user);
     Task DeleteAsync(string id);
     Task<User?> GetByEmailOrRegistrationAsync(string email, string registrationNumber);
 
