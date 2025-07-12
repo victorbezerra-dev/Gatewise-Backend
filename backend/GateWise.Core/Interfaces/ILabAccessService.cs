e8fa73215cd91435bf05446c1c524e64c355c41a
victor.dev <joaovictorbezerra.dev@gmail.com>
2025-07-12 14:43:33 -0400

    refactor: Rename IAccessLabService to ILabAccessService for consistency

--- backend/GateWise.Core/Interfaces/ILabAccessService.cs
+++ backend/GateWise.Core/Interfaces/ILabAccessService.cs
@@ -2,7 +2,7 @@ using GateWise.Core.DTOs;
 
 namespace GateWise.Core.Interfaces;
 
-public interface IAccessLabService
+public interface ILabAccessService
 {
     Task<string> RequestLabAccessAsync	(string userId, AccessLogCreateDto dto);
 }
