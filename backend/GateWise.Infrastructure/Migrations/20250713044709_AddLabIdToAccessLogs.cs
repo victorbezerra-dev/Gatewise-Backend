34f7b06a3226021dca0c3df63cf350827ebf7fe4
victor.dev <joaovictorbezerra.dev@gmail.com>
2025-07-13 03:59:13 -0400

    feat: Add migrations to add labId in acess log table

---
+++ backend/GateWise.Infrastructure/Migrations/20250713044709_AddLabIdToAccessLogs.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GateWise.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLabIdToAccessLogs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "user_id",
+                table: "access_logs",
+                type: "varchar(36)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "lab_id",
+                table: "access_logs",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_access_logs_lab_id",
+                table: "access_logs",
+                column: "lab_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_access_logs_user_id",
+                table: "access_logs",
+                column: "user_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_access_logs_labs_lab_id",
+                table: "access_logs",
+                column: "lab_id",
+                principalTable: "labs",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_access_logs_users_user_id",
+                table: "access_logs",
+                column: "user_id",
+                principalTable: "users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_access_logs_labs_lab_id",
+                table: "access_logs");
+
+            migrationBuilder.DropForeignKey(
+                name: "fk_access_logs_users_user_id",
+                table: "access_logs");
+
+            migrationBuilder.DropIndex(
+                name: "ix_access_logs_lab_id",
+                table: "access_logs");
+
+            migrationBuilder.DropIndex(
+                name: "ix_access_logs_user_id",
+                table: "access_logs");
+
+            migrationBuilder.DropColumn(
+                name: "lab_id",
+                table: "access_logs");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "user_id",
+                table: "access_logs",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(36)");
+        }
+    }
+}
